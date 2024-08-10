020dd76990e88e3f47fa2d134e72a3d949665011
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-10 22:42:47 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    char S, Q;
+    cin >> A >> S >> B >> Q >> C;
+
+    int result;
+    if (S == '+') {
+        result = A + B;
+    } else if (S == '-') {
+        result = A - B;
+    } else {
+        result = A * B;
+    }
+
+    if (result == C) {
+        cout << "Yes" << endl;
+    } else {
+        cout << result << endl;
+    }
+
+    return 0;
+}
